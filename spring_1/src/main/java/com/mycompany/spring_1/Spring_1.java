496392b0c06c2ff155229ac52f0b089ac8da6f48
daniel <ignacioleiva79@gmail.com>
2024-02-25 12:54:59 -0300

    esto es una prueba

---
+++ spring_1/src/main/java/com/mycompany/spring_1/Spring_1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.spring_1;
+
+/**
+ *
+ * @author dani
+ */
+public class Spring_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!2");
+    }
+}
